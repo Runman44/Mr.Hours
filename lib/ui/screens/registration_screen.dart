c87e0f481169966668d7fcef791185fa67de5f8c
Dennis Anderson <dennis@tellow.nl>
2020-09-14 13:33:59 +0200

    fix state error

--- lib/ui/screens/registration_screen.dart
+++ lib/ui/screens/registration_screen.dart
@@ -1,7 +1,6 @@
 import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 import 'package:eventtracker/bloc/ClientBloc.dart';
 import 'package:eventtracker/bloc/RegistrationBloc.dart';
-import 'package:eventtracker/ui/widgets/Loading.dart';
 import 'package:eventtracker/model/model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -25,8 +24,7 @@ class RegistrationEditor extends StatefulWidget {
       @required this.clientName,
       @required this.registration,
       @required this.startDate,
-      @required this.endDate
-      })
+      @required this.endDate})
       : super(key: key);
 
   @override
@@ -57,84 +55,82 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
 
   @override
   Widget build(BuildContext context) {
-
     var projects = _dropdownValue?.projects ?? [];
 
-    return BlocBuilder<ClientBloc, ClientState>(
-      builder: (BuildContext context, ClientState clientState) {
-        if (clientState is ClientsLoadSuccess) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(
-                widget.registration == null
-                    ? "Uren toevoegen"
-                    : "Uren wijzigen",
-              ),
-              flexibleSpace: Container(
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [
-                      Theme
-                          .of(context)
-                          .primaryColor,
-                      Theme
-                          .of(context)
-                          .accentColor
-                    ],
-                  ),
-                ),
-              ),
-              actions: <Widget>[
-                // action button
-                Visibility(
-                  visible: widget.registration != null,
-                  child: IconButton(
-                    icon: Icon(Icons.delete),
-                    onPressed: () async {
-                      BlocProvider.of<RegistrationBloc>(context).add(
-                        RemoveRegistration(widget.registration),
-                      );
-                      Navigator.pop(context);
-                    },
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.check),
-                  onPressed: () async {
-                    bool valid = _formKey.currentState.validate();
-                    if (!valid) return;
-
-                    _startDateTime = DateTime(_startDateTime.year, _startDateTime.month, _startDateTime.day, _startDateTime.hour, _startDateTime.minute);
-                    _endDateTime = DateTime(_endDateTime.year, _endDateTime.month, _endDateTime.day, _endDateTime.hour, _endDateTime.minute);
-
-                    if (widget.clientId != null &&
-                        widget.projectId != null &&
-                        widget.registration != null) {
-                      BlocProvider.of<RegistrationBloc>(context).add(
-                          EditRegistration(
-                              widget.registration,
-                              widget.clientId,
-                              widget.projectId,
-                              _startDateTime,
-                              _endDateTime,
-                              0));
-                    } else {
-                      BlocProvider.of<RegistrationBloc>(context).add(
-                          CreateRegistration(
-                              _dropdownValue,
-                              _dropdownValue2,
-                              _startDateTime,
-                              _endDateTime,
-                              0,
-                              _dropdownValue2.rate,
-                              _dropdownValue2.billable));
-                    }
-                    Navigator.pop(context);
-                  },
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          widget.registration == null ? "Uren toevoegen" : "Uren wijzigen",
+        ),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Theme.of(context).primaryColor,
+                Theme.of(context).accentColor
               ],
             ),
-            body: Padding(
+          ),
+        ),
+        actions: <Widget>[
+          // action button
+          Visibility(
+            visible: widget.registration != null,
+            child: IconButton(
+              icon: Icon(Icons.delete),
+              onPressed: () async {
+                BlocProvider.of<RegistrationBloc>(context).add(
+                  RemoveRegistration(widget.registration),
+                );
+                Navigator.pop(context);
+              },
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.check),
+            onPressed: () async {
+              bool valid = _formKey.currentState.validate();
+              if (!valid) return;
+
+              _startDateTime = DateTime(
+                  _startDateTime.year,
+                  _startDateTime.month,
+                  _startDateTime.day,
+                  _startDateTime.hour,
+                  _startDateTime.minute);
+              _endDateTime = DateTime(_endDateTime.year, _endDateTime.month,
+                  _endDateTime.day, _endDateTime.hour, _endDateTime.minute);
+
+              if (widget.clientId != null &&
+                  widget.projectId != null &&
+                  widget.registration != null) {
+                BlocProvider.of<RegistrationBloc>(context).add(EditRegistration(
+                    widget.registration,
+                    widget.clientId,
+                    widget.projectId,
+                    _startDateTime,
+                    _endDateTime,
+                    0));
+              } else {
+                BlocProvider.of<RegistrationBloc>(context).add(
+                    CreateRegistration(
+                        _dropdownValue,
+                        _dropdownValue2,
+                        _startDateTime,
+                        _endDateTime,
+                        0,
+                        _dropdownValue2.rate,
+                        _dropdownValue2.billable));
+              }
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+      body: BlocBuilder<ClientBloc, ClientState>(
+        builder: (BuildContext context, ClientState clientState) {
+          if (clientState is ClientsLoadSuccess) {
+            return Padding(
               padding: const EdgeInsets.all(24.0),
               child: Form(
                 key: _formKey,
@@ -150,24 +146,21 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
                       child: DropdownButton(
                         isExpanded: true,
                         icon: Icon(Icons.keyboard_arrow_down),
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .bodyText2,
+                        style: Theme.of(context).textTheme.bodyText2,
                         disabledHint: Text(widget.clientName ?? ""),
                         hint: Text("Kies een opdrachtgever"),
                         value: _dropdownValue,
                         onChanged: widget.clientId == null
                             ? (newValue) {
-                          setState(() {
-                            _dropdownValue = newValue;
-                          });
-                        }
+                                setState(() {
+                                  _dropdownValue = newValue;
+                                });
+                              }
                             : null,
                         underline: Container(),
-                        items: (BlocProvider
-                            .of<ClientBloc>(context)
-                            .state as ClientsLoadSuccess).clients
+                        items: (BlocProvider.of<ClientBloc>(context).state
+                                as ClientsLoadSuccess)
+                            .clients
                             .map<DropdownMenuItem<Client>>((Client value) {
                           return DropdownMenuItem<Client>(
                             value: value,
@@ -176,7 +169,9 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
                         }).toList(),
                       ),
                     ),
-                    SizedBox(height: 12,),
+                    SizedBox(
+                      height: 12,
+                    ),
                     Container(
                       padding: EdgeInsets.only(
                           left: 30, right: 30, top: 5, bottom: 5),
@@ -190,16 +185,13 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
                         value: _dropdownValue2,
                         icon: Icon(Icons.keyboard_arrow_down),
                         isExpanded: true,
-                        style: Theme
-                            .of(context)
-                            .textTheme
-                            .bodyText2,
+                        style: Theme.of(context).textTheme.bodyText2,
                         onChanged: widget.projectId == null
                             ? (newValue) {
-                          setState(() {
-                            _dropdownValue2 = newValue;
-                          });
-                        }
+                                setState(() {
+                                  _dropdownValue2 = newValue;
+                                });
+                              }
                             : null,
                         items: projects
                             .map<DropdownMenuItem<Project>>((Project value) {
@@ -210,14 +202,17 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
                         }).toList(),
                       ),
                     ),
-                    SizedBox(height: 24,),
-
+                    SizedBox(
+                      height: 24,
+                    ),
                     Text(
                       "Wanneer ben je begonnen?",
                       style:
-                      TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(height: 12,),
+                    SizedBox(
+                      height: 12,
+                    ),
                     DateTimeField(
                       format: DateFormat("yyyy-MM-dd HH:mm"),
                       onShowPicker: (context, currentValue) async {
@@ -240,19 +235,22 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
                       autovalidate: true,
                       validator: (date) => date == null ? 'Invalid date' : null,
                       initialValue: _startDateTime,
-                      onChanged: (date) =>
-                          setState(() {
-                            _startDateTime = date;
-                          }),
+                      onChanged: (date) => setState(() {
+                        _startDateTime = date;
+                      }),
                       resetIcon: Icon(Icons.delete),
                     ),
-                    SizedBox(height: 12,),
+                    SizedBox(
+                      height: 12,
+                    ),
                     Text(
                       "Wanneer ben je gestopt?",
                       style:
-                      TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                     ),
-                    SizedBox(height: 12,),
+                    SizedBox(
+                      height: 12,
+                    ),
                     DateTimeField(
                       format: DateFormat("yyyy-MM-dd HH:mm"),
                       onShowPicker: (context, currentValue) async {
@@ -275,20 +273,26 @@ class _RegistrationEditorState extends State<RegistrationEditor> {
                       autovalidate: true,
                       validator: (date) => date == null ? 'Invalid date' : null,
                       initialValue: _endDateTime,
-                      onChanged: (date) =>
-                          setState(() {
-                            _endDateTime = date;
-                          }),
+                      onChanged: (date) => setState(() {
+                        _endDateTime = date;
+                      }),
                       resetIcon: Icon(Icons.delete),
                     ),
                   ],
                 ),
               ),
-            ),
-          );
-        }
-        return Container();
-      },
+            );
+          }
+          if (clientState is ClientsLoadEmpty) {
+            return Center(
+              child: QuickSup.empty(
+                subtitle: 'Nog geen clienten aangemaakt',
+              ),
+            );
+          }
+          return Container();
+        },
+      ),
     );
   }
 }
